2fb4b2cb38ee9daa5cfefceeee33036a398c835a
rakgoorak <rakgoorak@gmail.com>
2024-01-31 17:04:11 +0700

    Up3

--- client/src/components/pages/Homepage.js
+++ client/src/components/pages/Homepage.js
@@ -2,11 +2,13 @@ import React from 'react'
 import NewProduct from '../home/NewProduct'
 import BastSeller from '../home/BastSeller'
 import Footer from '../pagehome/Footer'
+import Home from '../pagehome/Home'
 
 
 const Homepage = () => {
     return (
         <>
+            <Home />
             <h4 className='text-center p-3 mt-5 mb-5 display-4 jumbotron'>New Product</h4>
             <NewProduct />
             {/*New Product*/}
