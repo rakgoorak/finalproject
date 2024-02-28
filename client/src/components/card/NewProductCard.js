9b0b0e8a1d11672a06e1df02e3b1ef37c568f532
rakgoorak <rakgoorak@gmail.com>
2024-02-29 00:57:39 +0700

    0.57 29/2/2567

--- client/src/components/card/NewProductCard.js
+++ client/src/components/card/NewProductCard.js
@@ -3,7 +3,7 @@ import { Card } from 'antd';
 import { EyeOutlined, ShoppingCartOutlined } from '@ant-design/icons';
 import { Link } from 'react-router-dom';
 import _ from 'lodash';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { toast } from 'react-toastify';
 
 const { Meta } = Card;
@@ -21,39 +21,39 @@ const NewProductCard = ({ product }) => {
             ...product,
             count: 1
         });
-    let unique = _.uniqWith(cart, _.isEqual);
-    localStorage.setItem("cart", JSON.stringify(unique));
-    dispatch({
-        type: "ADD_TO_CART",
-        payload: unique
-    });
-    console.log('Cart after adding:', unique);
-    toast.success('เพิ่มสินค้าลงในตะกร้าสำเร็จ');
-};
+        let unique = _.uniqWith(cart, _.isEqual);
+        localStorage.setItem("cart", JSON.stringify(unique));
+        dispatch({
+            type: "ADD_TO_CART",
+            payload: unique
+        });
+        console.log('Cart after adding:', unique);
+        toast.success('เพิ่มสินค้าลงในตะกร้าสำเร็จ');
+    };
 
-return (
-    <Card
-        hoverable
-        style={{ width: '240px', marginBottom: '50px' }}
-        cover={<img
-            className='P-1'
-            style={{ height: '240px', objectFit: 'cover' }}
-            alt="example" src={images && images.length
-                ? images[0].url
-                : ''
-            } />}
-        actions={[
-            <Link to={`/product/${_id}`}>
-                <EyeOutlined className='text-warning' style={{ fontSize: '20px' }} />
-            </Link>,
-            <ShoppingCartOutlined style={{ fontSize: '20px' }}
-                onClick={handleAddtoCart}
-                className='text-danger' />,
-        ]}
-    >
-        <Meta title={name} description={detail} />
-    </Card>
-);
+    return (
+        <Card
+            hoverable
+            style={{ width: '240px', marginBottom: '50px' }}
+            cover={<img
+                className='P-1'
+                style={{ height: '240px', objectFit: 'cover' }}
+                alt="example" src={images && images.length
+                    ? images[0].url
+                    : ''
+                } />}
+            actions={[
+                <Link to={`/product/${_id}`}>
+                    <EyeOutlined className='text-warning' style={{ fontSize: '20px' }} />
+                </Link>,
+                <ShoppingCartOutlined style={{ fontSize: '20px' }}
+                    onClick={handleAddtoCart}
+                    className='text-danger' />,
+            ]}
+        >
+            <Meta title={name} description={detail} />
+        </Card>
+    );
 };
 
 export default NewProductCard;
