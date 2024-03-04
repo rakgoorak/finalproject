826e1f34bf249f05afafd57f591194f6524a177b
rakgoorak <rakgoorak@gmail.com>
2024-03-05 03:04:08 +0700

    3.03 5/3/2567

--- Server/config/db.js
+++ Server/config/db.js
@@ -1,26 +1,26 @@
-// const mongoose = require("mongoose");
-
-// const connectDB = async () => {
-//   try {
-//       const uri = process.env.DATABASEDEPLOY || 'mongodb+srv://rakgorak:Lr6BhgkKdI0VXPkY@cluster0.iwhsxw0.mongodb.net/db_elect';
-//       await mongoose.connect(uri);
-//       console.log("DB Connected");
-//   } catch (err) {
-//       console.error(err);
-//   }
-// };
-
-// module.exports = connectDB;
-
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/elect");
+    const uri = process.env.DATABASEDEPLOY || 'mongodb+srv://rakgorak:Lr6BhgkKdI0VXPkY@cluster0.iwhsxw0.mongodb.net/db_elect';
+    await mongoose.connect(uri);
     console.log("DB Connected");
   } catch (err) {
-    console.log(err);
+    console.error(err);
   }
 };
 
 module.exports = connectDB;
+
+// const mongoose = require("mongoose");
+
+// const connectDB = async () => {
+//   try {
+//     await mongoose.connect("mongodb://127.0.0.1:27017/elect");
+//     console.log("DB Connected");
+//   } catch (err) {
+//     console.log(err);
+//   }
+// };
+
+// module.exports = connectDB;
